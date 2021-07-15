7e5443a8995890054a9f91cb5d08d902741b2c03
micouti <86385110+micouti@users.noreply.github.com>
2021-07-15 12:47:08 -0300

    Modificando generos

---
+++ Pessoa.java
@@ -0,0 +1,112 @@
+package br.com.generation.projeto;
+
+public class Pessoa {
+	
+	private String nome;
+	private int idade;
+	private String rg;
+	private String cpf;
+	private int genero;
+	private int endereco;
+	private int numCasa;
+	private String telefone;
+	private String email;
+	private int trabalha;
+	private int pessoasCasa;
+	private double rendaCasa;
+	private int ensino;
+	private String curso;
+	private int etnia;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public int getGenero() {
+		return genero;
+	}
+	public void setGenero(int genero) {
+		this.genero = genero;
+	}
+	public int getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(int endereco) {
+		this.endereco = endereco;
+	}
+	public int getNumCasa() {
+		return numCasa;
+	}
+	public void setNumCasa(int numCasa) {
+		this.numCasa = numCasa;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getTrabalha() {
+		return trabalha;
+	}
+	public void setTrabalha(int trabalha) {
+		this.trabalha = trabalha;
+	}
+	public int getPessoasCasa() {
+		return pessoasCasa;
+	}
+	public void setPessoasCasa(int pessoasCasa) {
+		this.pessoasCasa = pessoasCasa;
+	}
+	public double getRendaCasa() {
+		return rendaCasa;
+	}
+	public void setRendaCasa(double rendaCasa) {
+		this.rendaCasa = rendaCasa;
+	}
+	public int getEnsino() {
+		return ensino;
+	}
+	public void setEnsino(int ensino) {
+		this.ensino = ensino;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	public int getEtnia() {
+		return etnia;
+	}
+	public void setEtnia(int etnia) {
+		this.etnia = etnia;
+	}
+
+}
